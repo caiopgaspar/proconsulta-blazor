9a45fa67b90a4d5d0b53eeee91913c68b5382f8d
caiopgaspar <c-gaspar@hotmail.com>
2024-08-22 23:34:28 -0300

    feat: cadastro de pacientes and update de paciente

--- ProConsulta/obj/Debug/net8.0/ProConsulta.AssemblyInfo.cs
+++ ProConsulta/obj/Debug/net8.0/ProConsulta.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProConsulta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff7891b8202a96210acb79dbd694b86718ecf24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89253199e71af7f2a21f6d4a93546f891b2dd807")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProConsulta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProConsulta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
